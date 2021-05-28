1c86777db8ac74ac359cc5e6cf3286ed3e9dee41
Justin Hamerly <suvimountain@icloud.com>
2021-05-27 22:58:58 -0700

    added loops and conditionals

--- app.js
+++ app.js
@@ -16,16 +16,44 @@ function getLizardBeauty(){
 }
 }
 
+let lizardskeleton = "https://wpclipart.com/dl.php?img=/animals/L/lizard/lizard_2/Flying_Dragon_skeleton__Agamidea_T.png";
+
+let lizardalive = "http://pngimg.com/uploads/lizard/lizard_PNG27.png";
+
+let tinylizard = "http://www.clker.com/cliparts/X/X/b/G/3/e/gecko-sil-th.png";
+
 function getLizardOnpage(){
     let lizardOnpage = prompt('Do you want to see your lizard?');
     console.log(lizardOnpage);
-
+ 
+    while(lizardOnpage.toLowerCase() != 'yes' && lizardOnpage.toLowerCase() != 'no'){
+        lizardOnpage = prompt('please answer yes or no');
+    }
     if (lizardOnpage.toLowerCase() == 'no'){
         document.write("<p>" + "<b>AHHH!</b>" + "</p>");
-        document.write("<img src=\"https://wpclipart.com/dl.php?img=/animals/L/lizard/lizard_2/Flying_Dragon_skeleton__Agamidea_T.png\" id=\"lizardImage\" onclick=\"flipImage();\"/>")
-    }
+        document.write("<img src=" + lizardskeleton + " id=\"lizardImage\" onclick=\"flipImage();\"/>")
+    } 
     else {
-        document.write("<img src=\"http://pngimg.com/uploads/lizard/lizard_PNG27.png\" id=\"lizardImage\" onclick=\"flipImage();\"/>")
+        document.write("<img src=" + lizardalive + " id=\"lizardImage\" onclick=\"flipImage();\"/>")
+    }
+}
+
+function getMoreLizards(){
+    let numberOfLizards = prompt("How many other lizards do you want to see?");
+    console.log(numberOfLizards);
+
+    while (numberOfLizards > 10 || numberOfLizards <0){
+        
+        if (numberOfLizards > 10){
+            numberOfLizards = prompt("We only have 10 lizards!  Please 10 or less");
+        }
+        else {
+            numberOfLizards = prompt("No space lizards... please enter at least 0 lizards.");
+        }   
+     }
+
+    for (let i=0; i < numberOfLizards; i++){
+        document.write("<img src=" + tinylizard + ">");
     }
 }
 
